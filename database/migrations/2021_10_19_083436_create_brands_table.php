36e3c2f1abc5e96e0ba93f8559d890bd7924124c
rollice2 <rollice2@gmail.com>
2021-10-19 16:35:15 +0700

    Phuoc V4

--- database/migrations/2021_10_19_083436_create_brands_table.php
+++ database/migrations/2021_10_19_083436_create_brands_table.php
@@ -13,19 +13,10 @@ class CreateBrandsTable extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD:database/migrations/2014_10_12_000000_create_users_table.php
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-=======
         Schema::create('brands', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('slug');
->>>>>>> 2b7d5cd5ca6f81bd9d579f58d815e67bc061187e:database/migrations/2021_10_19_083436_create_brands_table.php
             $table->timestamps();
         });
     }
