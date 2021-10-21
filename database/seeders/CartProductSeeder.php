e6f58a9a920ee60266a93f3ca35124b45a3be404
rollice2 <rollice2@gmail.com>
2021-10-21 21:10:12 +0700

    seeder v3

---
+++ database/seeders/CartProductSeeder.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Users\CartItem;
+
+class CartProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $item = new CartItem;
+        $item->id = 1;
+        $item->product_id = 1;
+        $item->quantity = 1;
+        $item->cart_id = 1;
+        $item->created_at = "2021-09-25 23:19:08";
+        $item->updated_at  = "2021-09-25 23:19:08";
+        $item->save();
+
+        $item = new CartItem;
+        $item->id = 2;
+        $item->product_id = 2;
+        $item->quantity = 1;
+        $item->cart_id = 2;
+        $item->created_at = "2021-09-25 23:19:08";
+        $item->updated_at  = "2021-09-25 23:19:08";
+        $item->save();
+
+        $item = new CartItem;
+        $item->id = 3;
+        $item->product_id = 3;
+        $item->quantity = 1;
+        $item->cart_id = 3;
+        $item->created_at = "2021-09-25 23:19:08";
+        $item->updated_at  = "2021-09-25 23:19:08";
+        $item->save();
+
+        $item = new CartItem;
+        $item->id = 4;
+        $item->product_id = 4;
+        $item->quantity = 1;
+        $item->cart_id = 4;
+        $item->created_at = "2021-09-25 23:19:08";
+        $item->updated_at  = "2021-09-25 23:19:08";
+        $item->save();
+
+        $item = new CartItem;
+        $item->id = 5;
+        $item->product_id = 5;
+        $item->quantity = 1;
+        $item->cart_id = 5;
+        $item->created_at = "2021-09-25 23:19:08";
+        $item->updated_at  = "2021-09-25 23:19:08";
+        $item->save();
+
+        $item = new CartItem;
+        $item->id = 6;
+        $item->product_id = 2;
+        $item->quantity = 1;
+        $item->cart_id = 6;
+        $item->created_at = "2021-09-25 23:19:08";
+        $item->updated_at  = "2021-09-25 23:19:08";
+        $item->save();
+
+        $item = new CartItem;
+        $item->id = 7;
+        $item->product_id = 1;
+        $item->quantity = 1;
+        $item->cart_id = 7;
+        $item->created_at = "2021-09-25 23:19:08";
+        $item->updated_at  = "2021-09-25 23:19:08";
+        $item->save();
+
+        $item = new CartItem;
+        $item->id = 8;
+        $item->product_id = 4;
+        $item->quantity = 1;
+        $item->cart_id = 8;
+        $item->created_at = "2021-09-25 23:19:08";
+        $item->updated_at  = "2021-09-25 23:19:08";
+        $item->save();
+
+        $item = new CartItem;
+        $item->id = 9;
+        $item->product_id = 3;
+        $item->quantity = 1;
+        $item->cart_id = 9;
+        $item->created_at = "2021-09-25 23:19:08";
+        $item->updated_at  = "2021-09-25 23:19:08";
+        $item->save();
+
+        $item = new CartItem;
+        $item->id = 10;
+        $item->product_id = 2;
+        $item->quantity = 1;
+        $item->cart_id = 10;
+        $item->created_at = "2021-09-25 23:19:08";
+        $item->updated_at  = "2021-09-25 23:19:08";
+        $item->save();
+    }
+}
