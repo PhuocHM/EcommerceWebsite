84eff17aeab62960218550fad16560482c9c1813
Hoàng Mạnh Phước <85532311+rollice2@users.noreply.github.com>
2021-10-25 15:13:26 +0700

    Hot fix Route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\Users\BestSellerController;
+use App\Http\Controllers\Users\CartController;
+use App\Http\Controllers\Users\CategoriesProductController;
+use App\Http\Controllers\Users\FlashSalesController;
+use App\Http\Controllers\Users\HomeController;
+
+use App\Http\Controllers\Users\RateProductsController;
+use App\Http\Controllers\Users\TrendingProductController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoriesController;
 use App\Http\Controllers\admin\ProductController;
@@ -17,32 +25,14 @@ use App\Http\Controllers\Admin\BrandsController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('Website.index');
-// });
-Route::get('login', function () {
-    return view('Website.login');
-});
-Route::get('category', function () {
-    return view('Website.grid-product');
-});
-Route::get('detail', function () {
-    return view('Website.detail');
-});
-Route::get('cart', function () {
-    return view('Website.shopping-cart');
-});
-Route::get('wishlist', function () {
-    return view('Website.wishlist');
-});
-Route::get('admin/home', function () {
-    return view('admin.dashboard.home');
-});
-Route::group(['prefix'=>'admin'],function() {
-    Route::resource('/categories',CategoriesController::class);
-    Route::resource('/attributes',AttributesController::class);
-    Route::resource('/brands',BrandsController::class);
-});
+Route::resource('/', HomeController::class);
+Route::resource('bestseller', BestSellerController::class);
+Route::resource('trending', TrendingProductController::class);
+
+Route::resource('flash-sales', FlashSalesController::class);
+Route::resource('rate-products', RateProductsController::class);
+Route::resource('maybe-you-like', MaybeYouLikeController::class);
+Route::resource('category', CategoriesProductController::class);
 
 Route::resource('cart', CartController::class);
 Route::GET('carts', [HomeController::class, 'addToCart'])->name('cart.addToCart');
